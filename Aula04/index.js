07ad67b6dfb72f520b381798e585660970806474
“Diego <diego.nascimento91@gmail.com>
2023-10-18 10:19:39 -0300

    Aula 04

---
+++ Aula04/index.js
@@ -0,0 +1,13 @@
+/*
+  Models
+    Criação de models e utilização no lugar de Interfaces
+  Diretivas
+    ngIg, ngClass, ngFor, ngModel ...
+    É possível criar diretrizes personalizadas (prox aula)
+  Data Binding
+    Interpolação -> {{ }}
+    Property binding -> [input]="input"
+    Event binding -> (click)='funcaoOnClick'
+    Two Way binding -> [(ngModel)]='variavel'
+      Se alterar a variavel, altera o valor do input, se alterar o valor do input, altera a variavel. Ou seja, os dados vão nos dois 'caminhos'
+*/
