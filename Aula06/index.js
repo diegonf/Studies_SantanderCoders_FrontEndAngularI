7970c4e6ae8c56b4454b1a9d84ca7eb66f170ada
“Diego <diego.nascimento91@gmail.com>
2023-10-25 19:06:24 -0300

    Aulas 05, 06 e 07 e proj até template-drive forms

---
+++ Aula06/index.js
@@ -0,0 +1,14 @@
+/* 
+  One-Way Data-Binding
+    É uma técnica que envolve a atualização unidirecional de dados do modelo para o template
+    Exemplos:
+      - Interpolação {{  }}
+      - Property-Binding []
+      - *ngIf e *ngFor2
+      - Event Binding
+      - Pipes
+  Two-Way Data Binding
+    Envolve atualização bidirecional de dados do modelo para o template
+    Exemplos:
+      - ngModel
+*/
---
+++ Aula07/index.js
@@ -0,0 +1,9 @@
+/* 
+  Template-driven forms
+    - formulários baseados em template
+    - abordagem de formulário no angular (tem duas abordagens)
+    - é adequado para formulários mais simples
+  Reactive forms
+    - segunda abordagem de formulário no angular
+    - ideal para validações no ts
+*/
